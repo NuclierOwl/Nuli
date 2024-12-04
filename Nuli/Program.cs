c5a5bc4016baf6ac45daa6dbcc68aa9e063c08e1
Userok <dullahannomer3@gmail.com>
2024-12-04 12:31:37 +0300

    Добавьте файлы проекта.

---
+++ Nuli/Program.cs
@@ -0,0 +1,27 @@
+﻿class Proga
+{
+    public static void Main() // основная часть
+    {
+        Console.WriteLine("Нужны цыфры: ");
+        int[] n = Array.ConvertAll(Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries), int.Parse); //сюда вводят значения игнорируя " ", ",", ";"
+        Peredvizenie(n);
+        Console.WriteLine("Итоговый массив: " + string.Join(" ", n));
+    }
+    public static int[] Peredvizenie(int[] nums) // пернгонщик нулей в конец
+    {
+        int poslednee = 0;
+        for (int i = 0; i < nums.Length; i++) // перенос всего что не "0" в начало
+        {
+            if (nums[i] != 0)
+            {
+                nums[poslednee] = nums[i];
+                poslednee++;
+            }
+        }
+        for (int i = poslednee; i < nums.Length; i++) // заполнение остатка нулями
+        {
+            nums[i] = 0;
+        }
+        return nums;
+    }
+}
